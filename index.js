fe64f3addfe804c45d9519b9c25e26875bf830d9
MusaCodeBB <musachm@code.edu.az>
2024-08-27 14:58:38 +0400

    Repeit commit

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+var addButton = document
+  .getElementsByClassName("input-container")[0]
+  .getElementsByTagName("button")[0];
+addButton.onclick = function () {
+  var taskInput = document.getElementById("taskInput").value;
+  var taskItem = document.createElement("div");
+  taskItem.className = "inputJS";
 
-var addButton = document.getElementsByClassName('input-container')[0].getElementsByTagName('button')[0];
-addButton.onclick = function() {
-    var taskInput = document.getElementById('taskInput').value;
-        var taskItem = document.createElement('div');
-        taskItem.className = 'inputJS';
+  var taskText = document.createElement("p");
+  taskText.textContent = taskInput;
 
-        var taskText = document.createElement('p');
-        taskText.textContent = taskInput;
+  var deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.style.backgroundColor = "yellow";
 
-        var deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        deleteButton.style.backgroundColor = 'yellow';
+  deleteButton.onclick = function () {
+    taskItem.remove();
+  };
 
-        deleteButton.onclick = function() {
-            taskItem.remove();
-        };
+  taskItem.appendChild(taskText);
+  taskItem.appendChild(deleteButton);
 
-        taskItem.appendChild(taskText);
-        taskItem.appendChild(deleteButton);
+  document.querySelector(".container").appendChild(taskItem);
 
-        document.querySelector('.container').appendChild(taskItem);
-
-        document.getElementById('taskInput').value = "";
-    }
-
+  document.getElementById("taskInput").value = "";
+  var deleteS = document.getElementsByClassName("delete");
+  for (let i = 0; i < deleteS.length; i++) 
+    {
+        deleteS[i].onclick=function(){
+            this.parentElement.remove();
+        }
+    };
+};
